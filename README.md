72c2d10c75278f00564643a2b0c5c10a1157681b
Tuhinadri Banerjee <tuhiniris@gmail.com>
2022-05-08 23:51:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mytest
+ My First Jenkins
---
+++ src/main/java/test/jenkins/mytest/App.java
@@ -0,0 +1,13 @@
+package test.jenkins.mytest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
